66f00ebd7441b1465ade4cf3312a421910167962
Van H <vphuynh@gmail.com>
2018-06-02 10:49:19 -0700

    Commented the vscode.interfaces.ts file

--- src/vscode.interfaces.ts
+++ src/vscode.interfaces.ts
@@ -1,3 +1,8 @@
+// creating an interface for the VS Code Extension window namespace so that 
+// intellisense can be used when the namespace is passed in as an argument
+// in the contructor of the duck-generator. this makes it easier to mock 
+// the window in the duck-generator test
+
 import { InputBoxOptions } from "vscode";
 
 export interface VSCodeWindow {
