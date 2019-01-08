43e8962fa18a741c927ebcf62aa3bf13d466c69d
vanister <vanister@users.noreply.github.com>
2019-01-07 22:03:07 -0800

    Added base-options object

---
+++ src/base-options.ts
@@ -0,0 +1,7 @@
+
+export const options = {
+  ext: '.js',
+  root: 'src/state/ducks',
+  files: ['operators', 'selectors', 'actions', 'reducers', 'types', 'test', 'index'],
+  additionalFiles: []
+};
