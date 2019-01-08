a104b63352dd5e4265671157c34f60b5c4c3db37
vanister <vanister@users.noreply.github.com>
2019-01-08 08:21:01 -0800

    Simplified config-util

--- src/utils/config-util.test.ts
+++ src/utils/config-util.test.ts
@@ -1,8 +1,7 @@
 import * as fs from 'fs';
-import * as path from 'path';
 
 import { getConfig } from './config-util';
-import { IOptions, IOptionOverrides } from '../models/options.interface';
+import { IOptions } from '../models/options.interface';
 
 jest.mock('fs');
 jest.mock('path');
@@ -14,61 +13,57 @@ const baseoptions: IOptions = {
   additionalFiles: ['one-more']
 };
 
-const root = '../test/';
-const file = 'ducks.config.js';
-const configPath = `${root}${file}`;
+const configPath = '../test/ducks.config.js';
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should error if path is empty, null or undefined', () => {
+  expect(() => { getConfig('', baseoptions); }).toThrowError('configPath');
+  expect(() => { getConfig(null as any, baseoptions); }).toThrowError('configPath');
+  expect(() => { getConfig(undefined as any, baseoptions); }).toThrowError('configPath');
+});
 
 test('should throw an error if base options are null or undefined', () => {
   let undefinedOptions: any;
   let nullOptions: any = null;
 
-  expect(() => { getConfig(nullOptions); }).toThrowError('baseOptions');
-  expect(() => { getConfig(undefinedOptions); }).toThrowError('baseOptions');
-  expect(() => { getConfig(baseoptions); }).not.toThrowError('baseOptions');
+  expect(() => { getConfig(configPath, nullOptions); }).toThrowError('baseOptions');
+  expect(() => { getConfig(configPath, undefinedOptions); }).toThrowError('baseOptions');
+  expect(() => { getConfig(configPath, baseoptions); }).not.toThrowError('baseOptions');
 });
 
 test('should throw an error if object is empty', () => {
-  expect(() => { getConfig({} as any); }).toThrowError('baseOptions');
+  expect(() => { getConfig(configPath, {} as any); }).toThrowError('baseOptions');
 });
 
 test('should throw an error if not an object', () => {
-  expect(() => { getConfig('object' as any); }).toThrowError('baseOptions');
-});
-
-test('should return a function wrapping the options', () => {
-  const withBaseOptions = getConfig(baseoptions);
-
-  expect(typeof withBaseOptions).toBe('function');
+  expect(() => { getConfig(configPath, 'object' as any); }).toThrowError('baseOptions');
 });
 
 test('should return the base config if config file is not found', () => {
   jest.spyOn(fs, 'existsSync').mockReturnValue(false);
-  jest.spyOn(path, 'resolve').mockReturnValue('does/not/exist/file.js');
 
-  const withBaseOptions = getConfig(baseoptions);
-  const retrievedOptions = withBaseOptions('does/not/exist/', 'file.js');
+  const options = getConfig('does/not/exist/file.js', baseoptions);
 
-  expect(retrievedOptions).toEqual(baseoptions);
+  expect(options).toEqual(baseoptions);
   expect(fs.existsSync).toHaveBeenCalledWith('does/not/exist/file.js');
-  expect(path.resolve).toHaveBeenCalledWith('does/not/exist/', 'file.js');
 });
 
 test('should return the options from config file', () => {
-  const configOptions: IOptionOverrides = {
+  const configOptions: IOptions = {
     ext: '.js',
     root: 'test/root',
     files: ['test', 'mock'],
     additionalFiles: ['two', 'files']
   };
 
-  jest.spyOn(path, 'resolve').mockReturnValue(configPath);
   jest.spyOn(fs, 'existsSync').mockReturnValue(true);
 
-  const withBaseOptions = getConfig(baseoptions);
-  const options = withBaseOptions(root, file);
+  const options = getConfig(configPath, baseoptions);
 
   expect(options).toEqual(configOptions);
-  expect(path.resolve).toHaveBeenCalledWith(root, file);
   expect(fs.existsSync).toHaveBeenCalledWith(configPath);
 });
 
@@ -78,13 +73,10 @@ test('should return merged options if config contains partial settings', () => {
     additionalFiles: ['only-one']
   };
 
-  jest.spyOn(path, 'resolve').mockReturnValue(configPath);
   jest.spyOn(fs, 'existsSync').mockReturnValue(true);
-
   jest.doMock(configPath, () => partialOptions);
 
-  const withBaseOptions = getConfig(baseoptions);
-  const options = withBaseOptions(root, file);
+  const options = getConfig(configPath, baseoptions);
 
   const expectedOptions = {
     ext: '.ts',
@@ -94,6 +86,5 @@ test('should return merged options if config contains partial settings', () => {
   };
 
   expect(options).toEqual(expectedOptions);
-  expect(path.resolve).toHaveBeenCalledWith(root, file);
   expect(fs.existsSync).toHaveBeenCalledWith(configPath);
 });
--- src/utils/config-util.ts
+++ src/utils/config-util.ts
@@ -1,23 +1,21 @@
-import * as path from 'path';
 import * as fs from 'fs';
-
 import { IOptions } from '../models/options.interface';
 
-export function getConfig(baseOptions: IOptions) {
+export function getConfig(configPath: string, baseOptions: IOptions): IOptions {
+  if (!configPath) {
+    throw new Error('configPath');
+  }
+
   if (!baseOptions || typeof baseOptions !== 'object' || Object.keys(baseOptions).length === 0) {
     throw new Error('baseOptions');
   }
 
-  return function (workspaceRoot: string, configFile: string = 'ducks.config.js') {
-    const fullpath = path.resolve(workspaceRoot, configFile);
+  if (!fs.existsSync(configPath)) {
+    return baseOptions;
+  }
 
-    if (!fs.existsSync(fullpath)) {
-      return baseOptions;
-    }
+  const config = require(configPath);
+  const merged = Object.assign({}, baseOptions, config) as IOptions;
 
-    const config = require(fullpath);
-    const merged = Object.assign({}, baseOptions, config);
-
-    return merged;
-  };
+  return merged;
 }
